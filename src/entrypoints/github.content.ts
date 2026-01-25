0a58a97c05d9ad64497dbcdd5f438bcc71eef51e
wlft <100052562+wlft@users.noreply.github.com>
2026-01-25 16:14:47 +1000

    fix(icons): support legacy source directory

--- src/entrypoints/github.content.ts
+++ src/entrypoints/github.content.ts
@@ -314,6 +314,7 @@ export default defineContentScript({
         const folder_icons: Record<string, string> = {
             // unlike file_icons, folder_icons entries should begin with a `.` if necessary
             'src': 'src.svg',
+            'source': 'src.svg',
             'docs': 'docs.svg',
 
             'public': 'public.svg',
